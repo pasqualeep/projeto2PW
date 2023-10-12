b6881e6fbf1dfb00637804cbcd04244760b11e99
PEDRO MIRANDA DE PASQUALE <pedro.mpasquale@senacsp.edu.br>
2023-10-11 22:33:03 -0300

    Adiçao dos cards

--- assets.js/carregar.js
+++ assets.js/carregar.js
@@ -1,6 +1,32 @@
 window.addEventListener("load", () => {
-    let tarefa = JSON.parse(localStorage.getItem("tarefas"))
-    tarefas.forEach((tarefas) =>{
-        console.log(tarefa)
-    })
+    let tarefa = JSON.parse(localStorage.getItem("tarefas")) || []
+
+    tarefas.forEach(tarefa => criarCard(tarefa))
+
 })
+
+function criarCard(tarefa) {
+    const card = document.createElement("div")
+
+    card.innerHTML = ` 
+    <div class="card">
+    <div class="card-content">
+        <span class="card-title"> ${tarefa.titulo} </span>
+        <p> ${tarefa.descricao} </p>
+        <span data-badge-caption="pontos" class="badge blue white-text">${tarefa.ponto}</span>
+    </div>
+    <div class="card-action">
+        <a href="#" class="btn red">
+            <i class="material-icons">delete_forever</i>
+        </a>
+        <a href="#" class="btn verde">
+            <i class="material-icons">check</i>
+        </a>
+    </div>
+</div>
+
+`
+
+document.querySelector("lista-tarefas").appendChild(card)
+
+}
