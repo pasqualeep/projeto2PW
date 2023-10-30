710378139cff5fafba1691b9eff58f8a3efd6225
PEDRO MIRANDA DE PASQUALE <pedro.mpasquale@senacsp.edu.br>
2023-10-30 20:01:24 -0300

    Projeto final

--- assets.js/dashboard.js
+++ assets.js/dashboard.js
@@ -5,17 +5,16 @@ function calcularEstatisticas(){
     const pendentes = tarefas.filter(t => t.concluida == false)
     const qtdePendentes = pendentes.lenght    
 
-    document.querySelector("#qtde-pendente").innerHTML = qtdePendentes
+    document.querySelector("#qtadep").innerHTML = qtdePendentes
 
     
 
     const concluidas = tarefas.filter(t => t.concluida == true)
     const qtdeConcluida = concluidas.lenght
     
-    document.querySelector("#qtde-concluida").innerHTML = qtdeConcluida
+    document.querySelector("#qtdec").innerHTML = qtdeConcluida
     
-    
-
-}
+   
 
 calcularEstatisticas()
+}
